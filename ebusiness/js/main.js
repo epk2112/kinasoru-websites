b1fac9b0dccbc09e6c66426434a9b9576c21577c
Erick Philbert <70050678+epk2112@users.noreply.github.com>
2024-09-10 12:27:54 +0000

    modified more

--- ebusiness/js/main.js
+++ ebusiness/js/main.js
@@ -252,6 +252,16 @@
     });
   });
 
+
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+    }
+  });
+
   /*---------------------
      Google Maps
   --------------------- */
